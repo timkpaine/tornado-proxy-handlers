04f0b44dc0b6a9a3a357114335ea78daa65194c3
Tim Paine <t.paine154@gmail.com>
2019-05-28 11:43:02 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requires = f.read().split()
+
+setup(
+    name='tornado-proxy-handlers',
+    version='0.0.1',
+    description='Tornado proxy handlers',
+    long_description=long_description,
+    url='https://github.com/timkpaine/tornado-proxy-handlers',
+    download_url='https://github.com/timkpaine/tornado-proxy-handlers/archive/v0.0.1.tar.gz',
+    author='Tim Paine',
+    author_email='timothy.k.paine@gmail.com',
+    license='Apache 2.0',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keywords='tornado proxy',
+    packages=find_packages(exclude=[]),
+    install_requires=requires,
+    extras_require={'dev': requires + ['pytest', 'pytest-cov', 'pylint', 'flake8']}
+)
